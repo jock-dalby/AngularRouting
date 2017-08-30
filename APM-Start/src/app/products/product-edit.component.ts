6e08db2ac94c8be8986bac75fd57c531ca1c6997
Jock Dalby <P765102@BNZNAG>
2017-08-31 09:11:10 +1200

    preserve params

--- APM-Start/src/app/products/product-edit.component.ts
+++ APM-Start/src/app/products/product-edit.component.ts
@@ -1,4 +1,4 @@
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 import { MessageService } from '../messages/message.service';
@@ -18,7 +18,8 @@ export class ProductEditComponent implements OnInit {
 
     constructor(private productService: ProductService,
                 private messageService: MessageService,
-                private route: ActivatedRoute) { }
+                private route: ActivatedRoute,
+                private router: Router) { }
 
     ngOnInit() {
         this.route.params.subscribe(
@@ -80,5 +81,6 @@ export class ProductEditComponent implements OnInit {
         }
 
         // Navigate back to the product list
+        this.router.navigate(['/products']);
     }
 }
